cbdf4d08e15f97e7bf4554cd5243d1bf56dde736
Petar Penev <ppenev@gatech.edu>
2019-12-07 17:52:49 -0500

    Fixing Pyrfu structure file.

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Correlating structural changes in the rRNA environment with phylogenetically derived measurements
 
 Contact maps were generated with RING 2.0 http://protein.bio.unipd.it/ring/
+With the following settings: 
+
+**Closest** network policy
+
+**Strict** distance thresholds
+
+**Multiple** interaction types
+
+**Skip water molecules**
--- bin/ccc_RNA.py
+++ bin/ccc_RNA.py
@@ -7,6 +7,8 @@ from Bio import AlignIO
 
 #%%
 def load_cons_data(csv_location,cons_data_dict):
+    '''Loads twincons data into provided dict
+    '''
     with open(csv_location) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -18,17 +20,21 @@ def load_cons_data(csv_location,cons_data_dict):
 
 #%%
 def gap_to_nogap_construct(alignIO_out_annotated):
+    '''Construct gap to nogap translation dictionary for given sequence id
+    '''
     nogap_ix=0
     nogap_to_gap_ix={}
     for ix,position in enumerate(alignIO_out_annotated):
         if position == '-':
             continue
         nogap_ix+=1
-        nogap_to_gap_ix[ix]=nogap_ix
+        nogap_to_gap_ix[ix+1]=nogap_ix
     return(nogap_to_gap_ix)
 
 #%%
 def get_annotation_sequence_index(seq_name,alignment):
+    '''Get the sequence id of provided sequence name and alignment.
+    '''
     for index, sequence in enumerate(alignment):
         if seq_name in sequence.id:
             annot_seq_ix = index
@@ -42,7 +48,15 @@ alignment = AlignIO.read(open('data/cons/LSU_AB.fas'), "fasta")
 
 gap_to_nogap_ecoli = gap_to_nogap_construct(alignment[get_annotation_sequence_index('LSUb_511145_ECOLI/1-2904',alignment)])
 gap_to_nogap_pyrfu = gap_to_nogap_construct(alignment[get_annotation_sequence_index('LSUa_186497_PYRFU/1-3048',alignment)])
-print(gap_to_nogap_pyrfu)
+#print(gap_to_nogap_pyrfu)
 cons_data_dict = dict()
 cons_data_dict = load_cons_data('data/cons/twc_AB_LSU.csv', cons_data_dict)
-#print(cons_data_dict)
+
+for aln_ix in cons_data_dict.keys():
+    try:
+        print('Alignment position:', aln_ix)
+        print ('ECOLI:', gap_to_nogap_ecoli[int(aln_ix)], cons_data_dict[aln_ix])
+        print ('PYRFU:', gap_to_nogap_pyrfu[int(aln_ix)], cons_data_dict[aln_ix])
+    except:
+        pass
+
